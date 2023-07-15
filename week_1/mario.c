6a8755509c262315bcc296199ff0a6820a37601a
Shazreen Seatter <shazreenseatter@Shazreens-MBP.broadband>
2023-07-15 16:28:38 +0100

    mario done

--- week_1/mario.c
+++ week_1/mario.c
@@ -1,8 +1,58 @@
 #include <stdio.h>
 #include <cs50.h>
 
+int get_size(void);
+void print_grid(int size);
 
+
+// Abstraction
 int main(void)
+{
+    // Get size of grid
+    int n = get_size();
+
+    // Print grid of bricks 
+    print_grid(n);
+}
+
+
+int get_size(void)
+{
+    int n;
+    do
+    {
+       n = get_int("Size: ");
+    } 
+    while (n < 1);
+    return n;
+}
+
+void print_grid(int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*int main(void) 
 {
     const int counter = 4; // const makes the code less vulnerable for unexpected change. better design 
 
@@ -14,4 +64,22 @@ int main(void)
         }
         printf("\n");
     }
-}
+
+    Get size of grid 
+    int n;
+    do
+    {
+       n = get_int("Size: ");
+    } 
+    while (n < 1);
+
+    // prints grid of bricks
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    } 
+} */
